c86c89337f0dc5eba89318a1b3ee6d1d14bd5e2f
mjerta <info@mpdev.nl>
2025-08-11 22:56:41 +0200

    Update scripts to make sure there are some extra checks

--- send-message-to-telegram-bot.sh
+++ send-message-to-telegram-bot.sh
@@ -3,6 +3,10 @@ if [ -z "$1" ]; then
   exit 1;
 fi
 
+if [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT_ID" ]; then
+  echo "Error: TELEGRAM_TOKEN and TELEGRAM_CHAT_ID environment variables must be set."
+  exit 1
+fi
 
 curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
      -d chat_id=${TELEGRAM_CHAT_ID} \
